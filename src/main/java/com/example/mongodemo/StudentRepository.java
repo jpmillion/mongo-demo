3bf8014ad03de38a2c6505f95afb69eeb520e0f5
John Million <john.million@rtslabs.com>
2022-11-22 12:01:05 -0500

    Create Student Repo

---
+++ src/main/java/com/example/mongodemo/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.mongodemo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+}
